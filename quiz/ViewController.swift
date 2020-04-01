7a3798f1cb0601a5954d127848029fb4fe1ff2c9
Kyurfour <kyurfor@gmail.com>
2020-04-02 00:32:57 +0300

    Initial Commit

---
+++ quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  quiz
+//
+//  Created by Vladimir Dyakov on 02/04/2020.
+//  Copyright © 2020 Vladimir Dyakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
